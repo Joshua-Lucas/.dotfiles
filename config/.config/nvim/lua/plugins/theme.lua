e49a2ffd5d368537df45a225c4faa5511367b408
Joshua Lucas <joshua@wholesalegorilla.net>
2024-03-29 10:26:55 -0400

    refactor(nvim): prettier formatting + theme colors

--- config/.config/nvim/lua/plugins/theme.lua
+++ config/.config/nvim/lua/plugins/theme.lua
@@ -1,22 +1,28 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "macchiato",
-        integrations = {
-          harpoon = true,
-          mason = true,
-          neotree = true,
-          lsp_trouble = true,
-        },
-      })
-      vim.cmd.colorscheme("catppuccin-macchiato")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				integrations = {
+					alpha = true,
+					harpoon = true,
+					mason = true,
+					neotree = true,
+					lsp_trouble = true,
+				},
+				color_overides = {
+					mocha = {
+						base = "#1e1e2e",
+					},
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
 --[[return {
   "rose-pine/neovim",
