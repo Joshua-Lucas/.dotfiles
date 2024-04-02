ef623d0e4275e5ce7ca1834fc2c7281520723fff
Joshua Lucas <joshua@wholesalegorilla.net>
2024-04-02 18:49:35 -0400

    refactor(nvim): update key maps for git merge file selection

--- config/.config/nvim/lua/plugins/git-plugs.lua
+++ config/.config/nvim/lua/plugins/git-plugs.lua
@@ -10,8 +10,8 @@ return {
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
             vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-            vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-            vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+            vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
+            vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")
         end
     }
 }
