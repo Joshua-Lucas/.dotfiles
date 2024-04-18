6d994bf0c090706bb1fa1739cd6eafcdc28a219c
Joshua Lucas <joshua@wholesalegorilla.net>
2024-04-18 09:54:49 -0400

    refactor(nvim): update remaps & add descriptions.

--- config/.config/nvim/lua/remaps.lua
+++ config/.config/nvim/lua/remaps.lua
@@ -1,21 +1,25 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Moves current line down a line" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Moves current line up a line" })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("x", "<leader>p", '"_dP')
+vim.keymap.set("x", "<leader>p", '"_dP', {
+	desc = "deletes text without affecting the default register contents and then paste it before the cursor position.",
+})
 
 -- Yanking remaps
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
+
 -- Remaps pv to open the explore on a vertical split
 vim.keymap.set("n", "<leader>pv", ":Vex<CR>")
 vim.keymap.set("n", "<leader>b%", ":vsplit<CR>")
+
 -- Sets leader + enter to source the file
 vim.keymap.set("n", "<Leader><CR>", ":so ~/.vimrc<CR>")
 
