631d1fc6225560647a33f927e567eb16beaaba17
Joshua Lucas <joshua@wholesalegorilla.net>
2025-09-08 07:40:50 -0400

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+
+ -- Set initial window size (e.g., 120 columns x 30 rows)
+  config.initial_cols = 120
+  config.initial_rows = 30
+
+
 -- Font Settings
 config.font = wezterm.font_with_fallback({
 	{
@@ -12,7 +18,7 @@ config.font = wezterm.font_with_fallback({
 	"JetBrains Mono",
 	{ family = "Symbols Nerd Font Mono", scale = 0.75 },
 })
-config.font_size = 12.5
+config.font_size = 15.5
 
 -- Theme
 config.color_scheme = "Catppuccin Mocha"
